2c6cf9db4e427057a9fb31d939055435a5eb4ca1
nuyhiu <uihyunk0305@gmail.com>
2026-01-01 20:25:52 +0900

    Refactor map setup and facility information
    Updated map initialization and facility details.

--- map.js
+++ map.js
@@ -1,63 +1,44 @@
-window.onload = function () {
-    var schoolPosition = new kakao.maps.LatLng(37.6435616, 127.2993351);
+var mapContainer = document.getElementById('map');
 
-    var map = new kakao.maps.Map(
-        document.getElementById('map'),
-        {
-            center: schoolPosition,
-            level: 4
-        }
-    );
-
-    new kakao.maps.Marker({
-        map: map,
-        position: schoolPosition
-    });
+var mapOption = {
+    center: new kakao.maps.LatLng(37.6435616, 127.2993351), 
+    level: 3
 };
+
+var map = new kakao.maps.Map(mapContainer, mapOption);
 
 var facilities = [
     {
         name: "○○ 장애인 복지관",
-        desc: "휠체어 접근 가능 · 상담 지원",
-        position: new kakao.maps.LatLng(37.5672, 126.9795)
+        address: "경기도 남양주시 ○○로 123",
+        type: "장애인 복지관",
+        position: new kakao.maps.LatLng(37.6542, 127.2451)
     },
     {
         name: "△△ 재활 센터",
-        desc: "물리치료 · 재활 프로그램",
-        position: new kakao.maps.LatLng(37.5658, 126.9772)
+        address: "경기도 남양주시 △△길 45",
+        type: "재활 치료 시설",
+        position: new kakao.maps.LatLng(37.6514, 127.2426)
     }
 ];
 
-var markers = [];
+var facilityList = document.getElementById("facility-list");
 
-facilities.forEach(function(facility) {
+facilities.forEach(function (facility) {
+
     var marker = new kakao.maps.Marker({
         map: map,
         position: facility.position
     });
 
-    var info = new kakao.maps.InfoWindow({
-        content: `
-            <div style="padding:5px;font-size:12px;">
-                <strong>${facility.name}</strong><br>
-                ${facility.desc}
-            </div>
-        `
-    });
-
     kakao.maps.event.addListener(marker, 'click', function () {
-        info.open(map, marker);
+
+        facilityList.innerHTML = `
+          <p>
+            <strong>${facility.name}</strong><br>
+            주소: ${facility.address}<br>
+            시설 유형: ${facility.type}
+          </p>
+        `;
     });
-
-    markers.push({ marker, info });
 });
-
-function moveTo(index) {
-    var target = facilities[index].position;
-    map.setCenter(target);
-    markers[index].info.open(map, markers[index].marker);
-}
-
-
-
-
